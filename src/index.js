026493d09442e1376c6671f8aa8657c0a47347f5
jhordan.followize <jhordan.suzuki@followize.com.br>
2023-09-08 14:04:11 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 const http = require('http')
 const PORT = 3000
+const DEFAULT_HEADER = {'Content-Type':'application/json'}
+
+const routes = {
+    '/heroes:get': async (request, response)=>{
+        const { id } = request.queryString
+        console.log({id})
+        return response.end()
+    },
+
+    default: (request, response)=>{
+        response.write('Hello my friend!');
+        response.end()
+    }
+}
 const handler = (request, response)=>{
-    response.end('Hey@')
+    const { url, method } = request
+    const [ first, route, id ] = url.split('/')
+
+    request.queryString = { id: isNaN(id) ? id : Number(id) }
+
+    const key = `/${route}:${method.toLowerCase()}`
+
+    response.writeHead(200, DEFAULT_HEADER)
+    
+    const chosen = routes[key] || routes.default
+    return chosen(request, response)
 }
 
 http.createServer(handler)
